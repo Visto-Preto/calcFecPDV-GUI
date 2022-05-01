c2180a75cde5b39a73d5df456a8a59eed7899f75
Visto-Preto <evppdepf@protonmail.ch>
2022-05-01 14:59:58 -0300

    add funcoes da tab 03

--- main.py
+++ main.py
@@ -20,6 +20,12 @@ j = valor
 k = valor
 l = valor
 
+rec = valor
+vda = valor
+vdap = valor
+ca = valor
+cac = valor
+
 def real(x):
 	return calc.str_real_format( calc.insert_result( x))
 
@@ -56,13 +62,13 @@ def main():
 
 	# Inicio da tab de calculos dos Recibos
 	layout_recibos = [	[sg.Text('Recibos', background_color=sg.theme_input_background_color(), text_color=sg.theme_input_text_color() ,justification='l', size=(15, 1), expand_x=True), sg.Input('', background_color=sg.theme_input_background_color(), text_color=sg.theme_input_text_color(), justification='r', size=(26, 1), expand_x=True, enable_events=True, key='-REC-' )],
-						[sg.Text('Venda avista', background_color=sg.theme_input_background_color(), text_color=sg.theme_input_text_color() ,justification='l', size=(15, 1), expand_x=True), sg.Input('', background_color=sg.theme_input_background_color(), text_color=sg.theme_input_text_color(), justification='r', size=(26, 1), expand_x=True)],
-						[sg.Text('Venda a prazo', background_color=sg.theme_input_background_color(), text_color=sg.theme_input_text_color() ,justification='l', size=(15, 1), expand_x=True), sg.Input('', background_color=sg.theme_input_background_color(), text_color=sg.theme_input_text_color(), justification='r',size=(26, 1), expand_x=True)],
-						[sg.Text('Cartão', background_color=sg.theme_input_background_color(), text_color=sg.theme_input_text_color() ,justification='l', size=(15, 1), expand_x=True), sg.Input(' ', background_color=sg.theme_input_background_color(), text_color=sg.theme_input_text_color(), justification='r', size=(26, 1), expand_x=True)],
-						[sg.Text('Carta crédito', background_color=sg.theme_input_background_color(), text_color=sg.theme_input_text_color() ,justification='l', size=(15, 1), expand_x=True), sg.Input('', background_color=sg.theme_input_background_color(), text_color=sg.theme_input_text_color(), justification='r', size=(26, 1), expand_x=True)],
+						[sg.Text('Venda avista', background_color=sg.theme_input_background_color(), text_color=sg.theme_input_text_color() ,justification='l', size=(15, 1), expand_x=True), sg.Input('', background_color=sg.theme_input_background_color(), text_color=sg.theme_input_text_color(), justification='r', size=(26, 1), expand_x=True, enable_events=True, key='-VDA-' )],
+						[sg.Text('Venda a prazo', background_color=sg.theme_input_background_color(), text_color=sg.theme_input_text_color() ,justification='l', size=(15, 1), expand_x=True), sg.Input('', background_color=sg.theme_input_background_color(), text_color=sg.theme_input_text_color(), justification='r',size=(26, 1), expand_x=True, enable_events=True, key='-VDAP-' )],
+						[sg.Text('Cartão', background_color=sg.theme_input_background_color(), text_color=sg.theme_input_text_color() ,justification='l', size=(15, 1), expand_x=True), sg.Input('', background_color=sg.theme_input_background_color(), text_color=sg.theme_input_text_color(), justification='r', size=(26, 1), expand_x=True, enable_events=True, key='-CA-' )],
+						[sg.Text('Carta crédito', background_color=sg.theme_input_background_color(), text_color=sg.theme_input_text_color() ,justification='l', size=(15, 1), expand_x=True), sg.Input('', background_color=sg.theme_input_background_color(), text_color=sg.theme_input_text_color(), justification='r', size=(26, 1), expand_x=True, enable_events=True, key='-CAC-' )],
 						[ sg.HorizontalSeparator()],
-						[sg.Text( 'Total Recebido', size=(15, 1), expand_x=True), sg.Text(valor, background_color=sg.theme_input_background_color(), text_color=sg.theme_input_text_color(), justification='r', size=(26, 1), expand_x=True)],
-						[sg.Text( 'Total Dinheiro', size=(15, 1), expand_x=True), sg.Text(valor, background_color=sg.theme_input_background_color(), text_color=sg.theme_input_text_color(), justification='r', size=(26, 1), expand_x=True)]
+						[sg.Text( 'Total Recebido', size=(15, 1), expand_x=True), sg.Text(valor, background_color=sg.theme_input_background_color(), text_color=sg.theme_input_text_color(), justification='r', size=(26, 1), expand_x=True, key='-DIS_TR-')],
+						[sg.Text( 'Total Dinheiro', size=(15, 1), expand_x=True), sg.Text(valor, background_color=sg.theme_input_background_color(), text_color=sg.theme_input_text_color(), justification='r', size=(26, 1), expand_x=True, key='-DIS_TD-')]
 						]
 	
 	# Inicio da tab de calculos do PDV
@@ -103,16 +109,14 @@ window = main()
 
 while True: 
 
-
-# Event Loop 
+	# Event Loop 
 	event, values = window.read() 
 
 	print(event)
 	if event in (None, 'Sair'): 
 		break
 
-# Eventos da tab 01
-
+	# Eventos da tab 01
 	# Incio da logica do campo de R$ 0,05
 	if event == '-IN005-' and len(values['-IN005-']) and values['-IN005-'][-1] not in ('0123456789'):
 		window['-IN005-'].update(values['-IN005-'][:-1])
@@ -188,9 +192,7 @@ while True:
 		e = valor
 		window['-OUT100-'].update(valor)
 
-
-# Eventos da tab 02
-
+	# Eventos da tab 02
 	# Incio da logica do campo de R$ 2,00	
 	if event == '-IN200-' and len(values['-IN200-']) and values['-IN200-'][-1] not in ('0123456789'):
 		window['-IN200-'].update(values['-IN200-'][:-1])
@@ -206,8 +208,7 @@ while True:
 		f = valor
 		window['-OUT200-'].update(valor)
 
-
-# Incio da logica do campo de R$ 5,00	
+	# Incio da logica do campo de R$ 5,00	
 	if event == '-IN500-' and len(values['-IN500-']) and values['-IN500-'][-1] not in ('0123456789'):
 		window['-IN500-'].update(values['-IN500-'][:-1])
 	if event == '-IN500-' and len(values['-IN500-']) and values['-IN500-'][-1] in ('0123456789'):
@@ -222,8 +223,7 @@ while True:
 		g = valor
 		window['-OUT500-'].update(valor)
 
-
-# Incio da logica do campo de R$ 10,00	
+	# Incio da logica do campo de R$ 10,00	
 	if event == '-IN1000-' and len(values['-IN1000-']) and values['-IN1000-'][-1] not in ('0123456789'):
 		window['-IN1000-'].update(values['-IN1000-'][:-1])
 	if event == '-IN1000-' and len(values['-IN1000-']) and values['-IN1000-'][-1] in ('0123456789'):
@@ -238,9 +238,7 @@ while True:
 		h = valor
 		window['-OUT1000-'].update(valor)
 
-
-
-# Incio da logica do campo de R$ 20,00	
+	# Incio da logica do campo de R$ 20,00	
 	if event == '-IN2000-' and len(values['-IN2000-']) and values['-IN2000-'][-1] not in ('0123456789'):
 		window['-IN2000-'].update(values['-IN2000-'][:-1])
 	if event == '-IN2000-' and len(values['-IN2000-']) and values['-IN2000-'][-1] in ('0123456789'):
@@ -255,7 +253,7 @@ while True:
 		i = valor
 		window['-OUT2000-'].update(valor)
 
-# Incio da logica do campo de R$ 50,00	
+	# Incio da logica do campo de R$ 50,00	
 	if event == '-IN5000-' and len(values['-IN5000-']) and values['-IN5000-'][-1] not in ('0123456789'):
 		window['-IN5000-'].update(values['-IN5000-'][:-1])
 	if event == '-IN5000-' and len(values['-IN5000-']) and values['-IN5000-'][-1] in ('0123456789'):
@@ -270,8 +268,7 @@ while True:
 		j = valor
 		window['-OUT5000-'].update(valor)
 
-
-# Incio da logica do campo de R$ 100,00	
+	# Incio da logica do campo de R$ 100,00	
 	if event == '-IN10000-' and len(values['-IN10000-']) and values['-IN10000-'][-1] not in ('0123456789'):
 		window['-IN10000-'].update(values['-IN10000-'][:-1])
 	if event == '-IN10000-' and len(values['-IN10000-']) and values['-IN10000-'][-1] in ('0123456789'):
@@ -286,8 +283,7 @@ while True:
 		k = valor
 		window['-OUT10000-'].update(valor)
 
-
-# Incio da logica do campo de R$ 200,00	
+	# Incio da logica do campo de R$ 200,00	
 	if event == '-IN20000-' and len(values['-IN20000-']) and values['-IN20000-'][-1] not in ('0123456789'):
 		window['-IN20000-'].update(values['-IN20000-'][:-1])
 	if event == '-IN20000-' and len(values['-IN20000-']) and values['-IN20000-'][-1] in ('0123456789'):
@@ -302,8 +298,8 @@ while True:
 		l = valor
 		window['-OUT20000-'].update(valor)
 
-# Eventos da tab 03
-
+	# Eventos da tab 03
+	# Eventos do campo de recibos
 	if event == '-REC-' and len(values['-REC-']) and values['-REC-'][-1] not in ('0123456789'):
 		window['-REC-'].update(values['-REC-'][:-1])
 	if event == '-REC-' and len(values['-REC-']) and values['-REC-'][-1] in ('0123456789'):
@@ -311,13 +307,121 @@ while True:
 			window['-REC-'].update(values['-REC-'][:-1])
 		if (len(values['-REC-']) < 20): #
 			if len(values['-REC-']) == 1:
-				y = int( values['-REC-'] )
-				window['-REC-'].update( rs.float_to_s(y) )
+				rec = int( values['-REC-'] )
+				window['-REC-'].update( rs.float_to_s(rec) )
 			else:
-				y = values['-REC-']
-				window['-REC-'].update( rs.del_caracter(y) )
+				rec = values['-REC-']
+				window['-REC-'].update( rs.del_caracter(rec) )
+
+	# Eventos do campo de venda avista
+	if event == '-VDA-' and len(values['-VDA-']) and values['-VDA-'][-1] not in ('0123456789'):
+		window['-VDA-'].update(values['-VDA-'][:-1])
+	if event == '-VDA-' and len(values['-VDA-']) and values['-VDA-'][-1] in ('0123456789'):
+		if (len(values['-VDA-']) > 19): # limite maximo de caracter 12
+			window['-VDA-'].update(values['-VDA-'][:-1])
+		if (len(values['-VDA-']) < 20): #
+			if len(values['-VDA-']) == 1:
+				vda = int( values['-VDA-'] )
+				window['-VDA-'].update( rs.float_to_s(vda) )
+			else:
+				vda = values['-VDA-']
+				window['-VDA-'].update( rs.del_caracter(vda) )
+
+	# Eventos do campo de venda a prazo
+	if event == '-VDAP-' and len(values['-VDAP-']) and values['-VDAP-'][-1] not in ('0123456789'):
+		window['-VDAP-'].update(values['-VDAP-'][:-1])
+	if event == '-VDAP-' and len(values['-VDAP-']) and values['-VDAP-'][-1] in ('0123456789'):
+		if (len(values['-VDAP-']) > 19): # limite maximo de caracter 12
+			window['-VDAP-'].update(values['-VDAP-'][:-1])
+		if (len(values['-VDAP-']) < 20): #
+			if len(values['-VDAP-']) == 1:
+				vdap = int( values['-VDAP-'] )
+				window['-VDAP-'].update( rs.float_to_s(vdap) )
+			else:
+				vdap = values['-VDAP-']
+				window['-VDAP-'].update( rs.del_caracter(vdap) )				
+
+	# Eventos do campo de cartao de credito
+	if event == '-CA-' and len(values['-CA-']) and values['-CA-'][-1] not in ('0123456789'):
+		window['-CA-'].update(values['-CA-'][:-1])
+	if event == '-CA-' and len(values['-CA-']) and values['-CA-'][-1] in ('0123456789'):
+		if (len(values['-CA-']) > 19): # limite maximo de caracter 12
+			window['-CA-'].update(values['-CA-'][:-1])
+		if (len(values['-CA-']) < 20): #
+			if len(values['-CA-']) == 1:
+				ca = int( values['-CA-'] )
+				window['-CA-'].update( rs.float_to_s(ca) )
+			else:
+				ca = values['-CA-']
+				window['-CA-'].update( rs.del_caracter(ca) )	
+
+	# Eventos do campo de carta credito
+	if event == '-CAC-' and len(values['-CAC-']) and values['-CAC-'][-1] not in ('0123456789'):
+		window['-CAC-'].update(values['-CAC-'][:-1])
+	if event == '-CAC-' and len(values['-CAC-']) and values['-CAC-'][-1] in ('0123456789'):
+		if (len(values['-CAC-']) > 19): # limite maximo de caracter 12
+			window['-CAC-'].update(values['-CAC-'][:-1])
+		if (len(values['-CAC-']) < 20): #
+			if len(values['-CAC-']) == 1:
+				cac = int( values['-CAC-'] )
+				window['-CAC-'].update( rs.float_to_s(cac) )
+			else:
+				cac = values['-CAC-']
+				window['-CAC-'].update( rs.del_caracter(cac) )	
+
+	# display somas dos recibos		
+	if type(rec) == int or type(vda) == int or type(vdap) == int or type(ca) == int or type(cac) == int :
+
+		if type(rec) == int:
+			rec = rs.float_to_s(rec)# int
+		else:
+			rec = rs.del_caracter(rec)# float
+
+		if type(vda) == int:
+			vda = rs.float_to_s(vda)# int
+		else:
+			vda = rs.del_caracter(vda)# float
+
+		if type(vdap) == int:
+			vdap = rs.float_to_s(vdap)# int
+		else:
+			vdap = rs.del_caracter(vdap)# float
 
 
+		if type(ca) == int:
+			ca = rs.float_to_s(ca)# int
+		else:
+			ca = rs.del_caracter(ca)# float
 
+		if type(cac) == int:
+			cac = rs.float_to_s(cac)# int
+		else:
+			cac = rs.del_caracter(cac)# float
+
+
+		dstr = ( rs.string_to_f(rec) + rs.string_to_f(vda) + rs.string_to_f(vdap) )
+		dstr = rs.float_to_s(dstr) 
+
+		dstd = ( (rs.string_to_f(rec) + rs.string_to_f(vda) + rs.string_to_f(vdap) ) - (rs.string_to_f(ca) + rs.string_to_f(cac) ) )
+		dstd = rs.float_to_s(dstd) 
+
+
+		window['-DIS_TR-'].update( dstr )
+		window['-DIS_TD-'].update( dstd )
+	else:
+		rec = rs.del_caracter(rec)# float
+		vda = rs.del_caracter(vda)# float
+		vdap = rs.del_caracter(vdap)# float
+		ca = rs.del_caracter(ca)# float
+		cac = rs.del_caracter(cac)# float
+
+		dstr = ( rs.string_to_f(rec) + rs.string_to_f(vda) + rs.string_to_f(vdap))
+		dstr = rs.float_to_s(dstr) 
+
+		dstd = ( (rs.string_to_f(rec) + rs.string_to_f(vda) + rs.string_to_f(vdap) ) - (rs.string_to_f(ca) + rs.string_to_f(cac) ) )
+		dstd = rs.float_to_s(dstd) 
+
+		window['-DIS_TR-'].update( dstr )
+		window['-DIS_TD-'].update( dstd )
 
 window.close(); del window
